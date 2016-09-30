a58e07d2224522ca1d3ac855596136a72aa4906a
kritigem16 <jainkritee@gmail.com>
2016-09-30 15:33:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,17 +87,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
 app.get('/:articleName',function(req,res){
    
    var articleName=req.params.articleName;
    res.send(createtemp(article[articleName]));
 });
 
-var counter=0;
-app.get('/counter',function(req,res){
-    counter=counter+1;
-    res.send(counter.toString());
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
