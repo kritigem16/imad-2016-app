826ab5bca9feb023281550d3539eae2ac5db4f42
kritigem16 <jainkritee@gmail.com>
2016-09-30 16:44:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,8 @@ button.oncick= function(){
      if(request.readyState===XMLHttpRequest.DONE)
      {
      if(request.status===200)
-    { var counter = request.responseText();
+    { 
+     var counter = request.responseText();
      var span = document.getElementById('count');
      span.innerHTML= counter.toString(); 
 }
@@ -18,3 +19,11 @@ button.oncick= function(){
    request.open('GET','http://kritigem16.imad.hasura-app.io/counter',true);
    request.send(null);
 };
+
+var nameInput=document.getElementById('name');
+var name= nameInput.value;
+var submit=document.getElementById('Submit');
+submit.onlcick=function(){
+    
+    
+}
