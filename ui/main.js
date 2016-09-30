374c903fee92ed70fd4e5ae5610a340efe7bf8cd
kritigem16 <jainkritee@gmail.com>
2016-09-30 11:59:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,8 @@ console.log('Loaded!');
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-    marginLeft+=8;
-    img.style.marginLeft=marginLeft+ 'px';
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft + 'px';
 }
 img.onclick= function (){
     var interval=setInterval(moveRight,100);
