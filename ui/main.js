2ada500ef8b150d8afec61350a110d22ac71004c
kritigem16 <jainkritee@gmail.com>
2016-09-30 11:54:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,12 @@
 console.log('Loaded!');
 //make the image move on click
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft+=8;
+    img.style.marginLeft=marginLeft+ 'px';
+}
 img.onclick= function (){
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,100);
+
 };
