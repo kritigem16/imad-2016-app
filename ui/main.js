1fba455be4be9e3038b7929feb8fc5db749a9b0a
kritigem16 <jainkritee@gmail.com>
2016-09-30 17:01:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,12 +24,26 @@ var nameInput=document.getElementById('name');
 var name= nameInput.value;
 var submit=document.getElementById('Submit');
 submit.onlcick=function(){
-    var names=[name1,name2,name3,name4];
+    
+    var request = new XMLHttpRequest();
+ request.onreadystatechange = function(){
+     if(request.readyState===XMLHttpRequest.DONE)
+     {
+     if(request.status===200)
+    { 
+      var names=request.responseText;
+      names=JSON.parsify(names);
     var list='';
     for(var i=0;i<names.length;i++)
     {
-        list+=<li>+names[i]+</li>;
+        list+='<li>'+names[i]+ '</li>'';
 }
 var ul=document.getElementById('nameList');
 ul.innerHTML= list;
+}
+}
+ };
+   request.open('GET','http://kritigem16.imad.hasura-app.io/submit-name/name'+name,true);
+   request.send(null);
+
 };
