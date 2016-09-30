b03a009396189151feb2e76c659dc95ab0e40a1a
kritigem16 <jainkritee@gmail.com>
2016-09-30 16:28:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,17 @@
 console.log('Loaded!');
 //counter
-var button=document.getElementById('counter');
+var button = document.getElementById('counter');
 
 button.oncick= function(){
 
- var request=new XMLHttpRequest();
- request.onreadystatechange=function(){
+ var request = new XMLHttpRequest();
+ request.onreadystatechange = function(){
      if(request.readyState===XMLHttpRequest.DONE)
      {
      if(request.status===200)
     { var counter = request.responseText();
-     var span= document.getElementById('count');
-     span.innerHTML=counter.toString(); 
+     var span = document.getElementById('count');
+     span.innerHTML= counter.toString(); 
 }
 }
  };
