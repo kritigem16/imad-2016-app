f8326704558b78b18107d9667aa93e2a24e919ec
kritigem16 <jainkritee@gmail.com>
2016-10-07 17:41:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,8 +103,22 @@ app.get('/:articleName',function(req,res){
    var articleName=req.params.articleName;
    res.send(createtemp(article[articleName]));
 });
+//storing comments in a 2-d array as [page][comments]
+var list=[[],[],[],[],[],[],[],[],[],[],[],[]];
 
-
+app.get('/ui/:id/comments', function (req, res)
+{
+  //url type: ui/3/comments?comment=... here id = 3
+  var id=req.params.id;
+  var id_no=parseInt(id,10);//convertin id containg string type  value to int type decimal value
+  id_no-=1;
+  var comment=req.query.comment;
+  if (comment!=="")
+    {list[id_no].push(comment);}
+  //returning only the row containing the comments from current page as JSON string represntation of that row,stored in a 2-D array
+  res.send(JSON.stringify(list[id_no]));
+});
+//lol
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
