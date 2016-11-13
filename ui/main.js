9f98a236813257876c2bc17fe7e7338367d4952d
kritigem16 <jainkritee@gmail.com>
2016-11-13 19:32:41 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -20,6 +20,36 @@ button.onclick= function(){
    request.send(null);
 };
 
+var submitComment = document.getElementById('submit_comment');
+submitComment.onclick = function () {
+    //create a request object
+    var request = new XMLHttpRequest();
+    //capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if (request.status === 200) {
+                //capture a list of comments and render it as a list
+               var comments = request.responseText;
+               comments = JSON.parse(comments);
+               var list = '';
+               for (var i=0; i<comments.length; i++) {
+                   list += '<li>' + comments[i] + '</li>';
+                   
+               }
+               var ul = document.getElementById('commentList');
+               ul.innerHTML = list;
+            }
+        }
+    };
+    
+     //make the request
+     var commentInput = document.getElementById('comment');
+     var comment = commentInput.value;
+     request.open('GET','http://kritigem16.imad.hasura-app.io/submit-comment?comment=' + comment, true);
+     request.send(null);
+    
+};
 
 var submit=document.getElementById('submit_btn');
 submit.onclick= function(){
