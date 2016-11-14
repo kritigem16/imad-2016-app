647c02060c1587a6971c13ade1d7d5ab8e4fc2d9
kritigem16 <jainkritee@gmail.com>
2016-11-14 14:04:16 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,31 @@
 
 //counter
 var button = document.getElementById('counter');
-
-button.onclick= function(){
-
- var request = new XMLHttpRequest();
- request.onreadystatechange = function() {
-     if(request.readyState===XMLHttpRequest.DONE)
-     {
-     if(request.status===200)
-    { 
-     var counter = request.responseText();
+ var counter = 0;
+  
+  button.onclick = function () {
+  
+      // Make a request to the counter endpoint
+     var request = new XMLHttpRequest();
+      
+      // Capture the response and store it in a variable
+     
+     // Render the variable in the correct span
+     counter = counter + 1;
      var span = document.getElementById('count');
-     span.innerHTML= counter.toString(); 
-}
-}
- };
+     span.innerHTML = counter.toString();
+     request.onreadystatechange = function () {
+       if (request.readyState === XMLHttpRequest.DONE) {
+           // Take some action
+           if (request.status === 200) {
+               var counter = request.responseText;
+               var span = document.getElementById('count');
+               span.innerHTML = counter.toString();          
+           }
+       }  
+       // Not done yet
+     };
+  };
    request.open('GET','http://kritigem16.imad.hasura-app.io/counter',true);
    request.send(null);
 };
@@ -51,29 +61,35 @@ submitComment.onclick = function () {
     
 };
 
-var submit=document.getElementById('submit_btn');
-submit.onclick= function(){
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
+  var submit = document.getElementById('submit_btn');
+  submit.onclick = function () {
     
+    var names = ['name1', 'name2', 'name3', 'name4'];
+   var list = '';
+   for (var i=0; i< names.length; i++) {
+       list += '<li>' + names[i] + '</li>';
+   }
+   var ul = document.getElementById('namelist');
+   ul.innerHTML = list;
     var request = new XMLHttpRequest();
- request.onreadystatechange = function(){
-     if(request.readyState===XMLHttpRequest.DONE)
-     {
-     if(request.status===200)
-    { 
-      var names=request.responseText;
-      names=JSON.parsify(names);
-    var list='';
-    for(var i=0;i<names.length;i++)
-    {
-        list+='<li>'+names[i]+ '</li>';
-}
-var ul=document.getElementById('nameList');
-ul.innerHTML= list;
-}
-}
- };
- var nameInput=document.getElementById('name');
- var name= nameInput.value;
+     
+     request.onreadystatechange = function () {
+       if (request.readyState === XMLHttpRequest.DONE) {
+           if (request.status === 200) {
+               var names = request.responseText;
+               names = JSON.parse(names);
+               var list = '';
+              for (var i=0; i< names.length; i++) {
+                   list += '<li>' + names[i] + '</li>';
+               }
+               var ul = document.getElementById('namelist');
+               ul.innerHTML = list;   
+           }
+       }  
+     };
+     
    request.open('GET','http://kritigem16.imad.hasura-app.io/submit-name?name='+name,true);
    request.send(null);
 };
