f26659cfa0ac372a50277e041d3833590b2ada74
kritigem16 <jainkritee@gmail.com>
2016-11-22 23:10:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,14 @@ var config = {
      port: '5432',
      password: process.env.DB_PASSWORD
  };
- 
+ var app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'someRandomSecretValue',
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
+}));
+
 
 function createTemplate (data) {
     var title = data.title;
