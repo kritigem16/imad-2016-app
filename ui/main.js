75834f0eb44e1e8b8b4632ca0d2ffcdd9b1cd227
kritigem16 <jainkritee@gmail.com>
2016-11-27 13:10:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -103,6 +103,21 @@ function loadLogin () {
     request.open('GET', '/check-login', true);
     request.send(null);
 }
+//site visit counter
+function getCounter(){
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+    if(request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+};
+ request.open('GET', '/counter', true);
+    request.send(null);
+}
 
 function loadArticles () {
         // Check if the user is already logged in
@@ -130,7 +145,7 @@ function loadArticles () {
     request.send(null);
 }
 
-
+getCounter();
 // The first thing to do is to check if the user is logged in!
 loadLogin();
 
