28f4912a1369674fb7695903464178195f1a6959
kritigem16 <jainkritee@gmail.com>
2016-11-27 13:13:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -210,7 +210,11 @@ app.post('/submit-comment/:articleName', function (req, res) {
         res.status(403).send('Only logged in users can comment');
     }
 });
-
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter = counter+1;
+   res.send(counter.toString()); 
+});
 app.get('/articles/:articleName', function (req, res) {
   // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
   pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
