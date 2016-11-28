b9314727b1db1dbc0d41e3c67f7772a2dbbec385
Kriti Jain <jainkritee@gmail.com>
2016-11-28 13:39:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -70,7 +70,9 @@ function createTemplate (data) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/news.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
 function hash (input, salt) {
     // How do we create a hash?
